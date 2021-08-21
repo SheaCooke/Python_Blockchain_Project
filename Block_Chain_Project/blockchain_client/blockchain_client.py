ec21241f0f31cfe4c32747d49451def589694885
shea cooke <SheaSCooke@gmail.com>
2021-08-21 11:13:10 -0700

    finshed JavaScript in view and controller code to generate public and private keys

--- Block_Chain_Project/blockchain_client/blockchain_client.py
+++ Block_Chain_Project/blockchain_client/blockchain_client.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
 
 class Transaction:
     def __init__(self, sender_address, sender_private_key, receipient_address, value):
@@ -24,7 +28,17 @@ def view_transactions():
 
 @app.route('/wallet/new')
 def new_wallet():
-    return ''
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.publickey()
+
+    response = {
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
+    }
+
+
+    return jsonify(response), 200 #needs to be converted into json format, using json file from flask, 200 = http for response succes
 
 
 if __name__ == '__main__': # check if file was ran as a script or imported as a moduel
