c652e6b00c19d7a2ec9ca138863c079050234a52
shea cooke <SheaSCooke@gmail.com>
2021-08-22 12:15:31 -0700

    added functionality to generate confirmation window after a transaction has been succesfully submited

--- Block_Chain_Project/blockchain/blockchain.py
+++ Block_Chain_Project/blockchain/blockchain.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from time import time
+from flask_cors import CORS
 
 class Blockchain:
     def __init__(self):
@@ -26,10 +27,21 @@ blockchain = Blockchain() #create an instance
 
 #instantiate the node with flask
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/') #same idea as a route in ASP.NET
 def index():
     return render_template('./index.html') #to translate to ASP.NET, this is a controller that renders a view called index.html
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    response = {'message':'Ok'}
+    return jsonify(response), 201 #201 http code for adding new resource
+
+
+
+
 
 
 if __name__ == '__main__': # check if file was ran as a script or imported as a moduel
