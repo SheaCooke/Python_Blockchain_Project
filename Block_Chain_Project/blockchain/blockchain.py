d0f5b191fdd740d385ba3c5abb9675f28d191743
shea cooke <SheaSCooke@gmail.com>
2021-08-22 13:16:21 -0700

    added functionality to add a new transaction to a list of transactions

--- Block_Chain_Project/blockchain/blockchain.py
+++ Block_Chain_Project/blockchain/blockchain.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from time import time
 from flask_cors import CORS
+from collections import OrderedDict
 
 class Blockchain:
     def __init__(self):
@@ -18,6 +19,27 @@ class Blockchain:
         }
         self.transactions = [] #reset current list of transactions
         self.chain.append(block)
+    def submit_transaction(self, sender_public_key, recipient_public_key, signature, amount):
+        # TODO: reward the miner
+        # TODO: signature validation
+
+        transaction = OrderedDict({
+            'sender_public_key': sender_public_key,
+            'recipient_public_key': recipient_public_key,
+            'signature': signature,
+            'amount':amount
+        })
+        signature_verification = True #verify the signature
+
+        if signature_verification:
+            self.transactions.append(transaction)
+            return len(self.chain) + 1
+
+        else:
+            return False
+
+
+
 
 
 
@@ -36,8 +58,18 @@ def index():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    response = {'message':'Ok'}
-    return jsonify(response), 201 #201 http code for adding new resource
+    values = request.form
+    transaction_results = blockchain.submit_transaction(values['confirmation_sender_public_key'], values['confirmation_recipient_public_key'], values['transaction_signature'], values['confirmation_amount'])
+
+    if transaction_results == False:
+        response = {'message': 'Invalid transaction/signature'}
+        return jsonify(response), 406
+
+    else:
+        response = {'message': 'Transaction will be added to the Block '+ str(transaction_results)}
+        return jsonify(response), 201
+
+
 
 
 
